4c66c0888ca28633535aab67c70d6b88c5b048ed
Lisboa <lisboadivalu@gmail.com>
2021-03-24 11:09:02 -0300

    update

--- src/components/Conteudo.js
+++ src/components/Conteudo.js
@@ -4,7 +4,9 @@ class Conteudo extends Component{
     render() {
         return(
             <div className=" pt-10 p-5 m-0 h-screen bg-gray-300">
+                
                 <div className=" pl-5 container mx-auto gap-3 gap-y-10 grid grid-cols-3 content-start place-item-auto justify-center">
+                    
                     <div className="max-w-xs rounded overflow-hidden shadow-lg bg-gray-200 object-right object">
                         <img className="w-full" src={ require('../assets/download.jpeg').default } alt="" />
                         <div className="px-6 py-4">
@@ -14,7 +16,9 @@ class Conteudo extends Component{
                             </p>
                         </div>
                     </div>
+                    
                 </div>
+
             </div>
         );
     }
