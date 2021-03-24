d0f5fff8d5a49ed783d51052229b06d3717c3b72
Lisboa <lisboadivalu@gmail.com>
2021-03-24 10:53:48 -0300

    customization

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return(
             <div>
-                <footer className=" static w-full bottom-0 h-14 bg-black">
+                <footer className=" w-full bottom-0 h-14 bg-black">
                         
                 </footer>
             </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,8 @@ class Nav extends Component {
     render() {
         return(
             <div>
-                <nav className="fixed w-full m-0 md:m-full h-16 bg-black">
+                <nav className="flex w-full m-0 md:m-full h-16 bg-black">
                     <div className="pl-5 h-10 font-sans font-bold text-lg text-white inline-block align-middle">
-                        
                     </div>
                 </nav>
             </div>
